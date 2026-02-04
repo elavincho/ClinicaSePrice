313a1ff3aba60c93e67f9de59b13f1a5533d117f
elavincho <elavincho@gmail.com>
2026-02-03 22:13:39 -0300

    Creo el proyecto

---
+++ seprice/src/main/java/com/clinicaseprice/seprice/SepriceApplication.java
@@ -0,0 +1,16 @@
+package com.clinicaseprice.seprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SepriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SepriceApplication.class, args);
+        
+        System.out.println("Proyecto: Clínica SePrice");
+        System.out.println("Servidor ejecutándose en: http://localhost:8080");
+    }
+}
---
+++ seprice/src/test/java/com/clinicaseprice/seprice/SepriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicaseprice.seprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
